8eae8ac14cc0ad33362e01e12718f9dd5a5dd479
kokchun <kokchungiang@gmail.com>
2026-08-18 11:32:16 +0200

    updated code

--- 00_setup_internal_led/main.py
+++ 00_setup_internal_led/main.py
@@ -6,10 +6,5 @@ sleep(1)
 led_internal = Pin("LED", 1)
 
 while True:
-    led_internal.value(1)
-
-    sleep(2)
-
-    led_internal.value(0)
-
-    sleep(2)
+    led_internal.toggle()
+    sleep(.5)

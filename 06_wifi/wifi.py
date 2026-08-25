d37cb925f73e75394222142e17c5b2ede397264a
kokchun <kokchungiang@gmail.com>
2026-08-25 13:44:44 +0200

    wifi connection code

--- 06_wifi/wifi.py
+++ 06_wifi/wifi.py
@@ -8,4 +8,21 @@ rp2.country("SE")
 with open("wifi_credentials.json") as file:
     credentials = json.load(file)
 
+def connect_wifi(waiting_time = 10):
+    # pico becomes a client to connect to WIFI
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True) # powers on radio WIFI on pico W/2W
+    wlan.connect(credentials.get("SSID"), credentials.get("PASSWORD"))
+
+    while waiting_time > 0:
+        if wlan.isconnected():
+            print("congraz u connected")
+            break
+
+        waiting_time -= 1
+        print("Try to connect to wifi, waitu a little bit")
+        time.sleep(2)
+
+    return wlan.isconnected()
+
 print(credentials)
